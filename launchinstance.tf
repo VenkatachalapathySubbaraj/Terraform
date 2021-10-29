735ce10c8a7b0150f0475516b40fd431e937e32f
VenkatachalapathySubbaraj <68412881+VenkatachalapathySubbaraj@users.noreply.github.com>
2021-10-29 08:56:31 +0530

    Update launchinstance.tf

--- launchinstance.tf
+++ launchinstance.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "testinstance" {
- ami = "ami-0d382e80be7ffdae5"
- instance_type = "t2.micro"
- key_name = "DockerContainer"
-}
+# resource "aws_instance" "testinstance" {
+#  ami = "ami-0d382e80be7ffdae5"
+#  instance_type = "t2.micro"
+#  key_name = "VenkatTomcat"
+# }
